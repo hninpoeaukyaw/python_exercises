5d2271a7a3e5b242dde95288d7962e912bcfd702
hninpoeaukyaw <hninpoeaukyaw.mdy@gmail.com>
2018-06-26 12:27:04 +0530

    change from dog to cat

--- ex101.py
+++ ex101.py
@@ -4,7 +4,11 @@ persian_cat = "\I'm split \non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
 fat_cat = """
-
+I'll do a list:
+\t* Cat food
+\t* finishes
+\t* Catnip\n\t* Grass
+"""
 print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
